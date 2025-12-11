0fd5a83afdcffa6cc521381404ad8fa6fd789c15
Blake Hutchinson <blake.hutchinson.1996@gmail.com>
2025-12-11 16:26:12 -0600

    Uploaded basic Cube scene

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+	.add_plugins(DefaultPlugins) // Loads the important plugins for rendering, input, and more.
+	.add_startup_system(setup) // Registers the setup system to run once at startup.
+	.run(); // Starts the application.
+}
+
+fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>) {
+	// Spawn a 3D camera that looks at the origin (0,0,0).
+	commands.spawn(Camera3dBundle {
+		transform: Transform::from_xyz(0.0, 8.0, 15.0)
+			.looking_at(Vec3::ZERO, Vec3::Y),
+		..default()
+	});
+
+	// Spawn a point light to illuminate the scene.
+	commands.spawn(PointLightBundle {
+		point_light: PointLight {
+			intensity: 1000.0,
+			shadows_enabled: true,
+			..default()
+		},
+		transform: Transform::from_xyz(4.0, 8.0, 4.0),
+		..default()
+	});
+
+	// Spawn a cube in the scene.
+	commands.spawn(PbrBundle {
+		mesh: meshes.add(Mesh::from(shape::Cube { size: 2.0 })),
+		material: materials.add(StandardMaterial {
+			base_color: Color::rgb(0.8, 0.2, 0.2),
+			..default()
+		}),
+		transform: Transform::from_xyz(0.0, 0.5, 0.0),
+		..default()
+	});
 }
